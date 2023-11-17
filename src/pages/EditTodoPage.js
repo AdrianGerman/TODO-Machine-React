5b30a223558b2b0b61cc7503e3a44e7acb67905f
AdrianGerman <adrian.german1019@gmail.com>
2023-11-17 09:37:08 -0600

    data transfer by browsing

--- src/pages/EditTodoPage.js
+++ src/pages/EditTodoPage.js
@@ -1,29 +1,35 @@
 import React from "react";
 import { TodoForm } from "../components/TodoForm";
 import { useTodos } from "../context/useTodos";
-import { useParams } from "react-router-dom";
+import { useLocation, useParams } from "react-router-dom";
 
 function EditTodoPage() {
+  const location = useLocation();
   const params = useParams();
   const id = Number(params.id);
 
   const { states, stateUpdaters } = useTodos();
-  const { editTodo } = stateUpdaters;
   const { loading, getTodo } = states;
+  const { editTodo } = stateUpdaters;
 
-  if (loading) {
+  let todoText;
+
+  if (location.state?.todo) {
+    todoText = location.state.todo.text;
+  } else if (loading) {
     return <p>Cargando....</p>;
   } else {
     const todo = getTodo(id);
-    return (
-      <TodoForm
-        label="Editar tarea"
-        defaultTodoText={todo.text}
-        submitText="Editar"
-        submitEvent={(newText) => editTodo(id, newText)}
-      />
-    );
+    todoText = todo.text;
   }
+  return (
+    <TodoForm
+      label="Editar tarea"
+      defaultTodoText={todoText}
+      submitText="Editar"
+      submitEvent={(newText) => editTodo(id, newText)}
+    />
+  );
 }
 
 export { EditTodoPage };
